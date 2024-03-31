884a4f36a55f2a0b4fdf5a19e9c52bb62b236196
mounirBen26 <mounir.bmh@hotmail.fr>
2024-03-31 15:17:15 +0100

    added table display

--- app/pages/devices/getDevices.tsx
+++ app/pages/devices/getDevices.tsx
@@ -3,30 +3,45 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 
-export default  function FetchDevices({devices}:any) {
-   const [deviceTerm, setDeviceTerm] = useState('')
-   const [deviceList, setDeviceList] = useState([])
-   
+export default function FetchDevices({ devices }: any) {
+  const [deviceTerm, setDeviceTerm] = useState('')
+  const [deviceList, setDeviceList] = useState([])
+
 
 
   return (
-    <div>
+    <div className='w-1/2'>
       <section>
-      <h1 className='text-xl text-blue-500 font-bold'>Search Devices</h1>
-          <input className='border-2 border-gray-300 rounded-md p-2 w-full my-2' 
-              type="text"
-              placeholder="Search Devices"
-              onChange={(e) =>{setDeviceTerm(e.target.value)} }
-          />
+        <h1 className='text-xl text-blue-500 font-bold'>Search Devices</h1>
+        <input className='border-2 border-gray-300 rounded-md p-2 w-full my-2'
+          type="text"
+          placeholder="Search Devices"
+          onChange={(e) => { setDeviceTerm(e.target.value) }}
+        />
       </section>
       <section>
-      {devices
-      .filter((device:any) => device.device.toLowerCase().includes(deviceTerm.toLowerCase()))
-      .map((device:any)=>{
+        <table className='w-full my-2 text-center border border-gray-300 rounded'>
+          <thead className='bg-gray-200 font-bold text-l'>
+            <tr>
+              <th>ID</th>
+              <th>Device</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+          {devices
+            .filter((device: any) => device.device.toLowerCase().includes(deviceTerm.toLowerCase()))
+            .map((device: any) => {
+              return <tr key={device.id}>
+                <td>{device.id}</td>
+                <td> {device.device}</td>
+                <td>{device.createdAt.slice(0, 10)}</td>
+              </tr>
+            }
+            )}</tbody>
+      </table>
+    </section>
 
-      return <p key={device.id} className="text-sm font-bold mb-2  hover:bg-blue-500 hover:text-white hover:scale-105 transition duration-100">{device.device} <span className='cursor-pointer bg-red-500 text-white px-2 rounded'><Link href={`/pages/devices/${device.id}`}>view</Link></span> </p> })}
-      </section>
-    
-    </div>
+    </div >
   )
 }
