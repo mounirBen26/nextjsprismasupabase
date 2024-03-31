5c889e8c2bb6e2b18208a9d7acd7abafc444760a
mounirBen26 <mounir.bmh@hotmail.fr>
2024-03-31 15:38:34 +0100

    added mesage: no data found if search is dont match

--- app/pages/devices/getDevices.tsx
+++ app/pages/devices/getDevices.tsx
@@ -7,7 +7,9 @@ export default function FetchDevices({ devices }: any) {
   const [deviceTerm, setDeviceTerm] = useState('')
   const [deviceList, setDeviceList] = useState([])
 
-
+  const filteredDevice = devices.filter((device:any)=>{
+    return device.device.toLowerCase().includes(deviceTerm.toLowerCase())
+  })
 
   return (
     <div className='w-1/2'>
@@ -29,16 +31,19 @@ export default function FetchDevices({ devices }: any) {
             </tr>
           </thead>
           <tbody>
-          {devices
-            .filter((device: any) => device.device.toLowerCase().includes(deviceTerm.toLowerCase()))
-            .map((device: any) => {
-              return <tr key={device.id}>
-                <td>{device.id}</td>
-                <td> {device.device}</td>
-                <td>{device.createdAt.slice(0, 10)}</td>
-              </tr>
-            }
-            )}</tbody>
+          {filteredDevice.length > 0 ? 
+          (filteredDevice.map((device: any) => {
+            return <tr key={device.id} className='border'>
+              <td className='p-1'>{device.id}</td>
+              <td> {device.device}</td>
+              <td>{device.createdAt.slice(0, 10)}</td>
+            </tr>
+          }
+          ))
+          :
+          (<tr  className='border'> <td colSpan={3}>No data found</td></tr>)
+            
+            }</tbody>
       </table>
     </section>
 
